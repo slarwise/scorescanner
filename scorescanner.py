9e6041592ab98670d7ffc601831f3ca8f0aa4c7b
Arvid Bjurklint <arvid.bjurklint@hotmail.com>
2021-08-11 13:01:48 +0200

    Set server_name so gradio works through docker

--- scorescanner.py
+++ scorescanner.py
@@ -5,7 +5,7 @@ import numpy as np
 GRADIO_COLOR_ORANGE = (231, 162, 59)
 def main():
     iface = gr.Interface(analyze_image, gr.inputs.Image(), "image",
-            examples=[["scores.png"]], server_port=8080)
+            examples=[["scores.png"]], server_port=8080, server_name="0.0.0.0")
     iface.launch()
     # show(analyze_image("scores.png"))
 
